cc66f45f4c185410ca50bd574f9a5d7795dd445e
WooMin <hanulbom@gmail.com>
2020-11-16 15:41:54 +0900

    update and delete

---
+++ src/components/Nweet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Nweet = ({ nweetObj }) => {
+  <div>
+    <h4>{nweetObj.text}</h4>
+  </div>;
+};
+
+export default Nweet;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -16,38 +16,37 @@ const Auth = () => {
       setPassword(value);
     }
   };
-  const onSubmit = async(event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
     try {
       let data;
-      if(newAccount){
+      if (newAccount) {
         data = await authService.createUserWithEmailAndPassword(
-          email, password
-        )
+          email,
+          password,
+        );
       } else {
-        data = await authService.signInWithEmailAndPassword(
-          email, password
-        )
+        data = await authService.signInWithEmailAndPassword(email, password);
       }
-      console.log(data)
-      } catch (error) {
-        setError(error.message);
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
     }
   };
-const toggleAccount = () => setNewAccount(prev => !prev);
-const onSocialClick = async (event) => {
-  const {
-    target: { name },
-  } = event;
-  let provider;
-  if (name === "google") {
-    provider = new firebaseInstance.auth.GoogleAuthProvider();
-  } else if (name === "github") {
-    provider = new firebaseInstance.auth.GithubAuthProvider();
-  }
-  const data = await authService.signInWithPopup(provider);
-  console.log(data);
-};
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
 
   return (
     <div>
@@ -68,13 +67,22 @@ const onSocialClick = async (event) => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
         {error}
       </form>
-      <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
       <div>
-        <button onClick={onSocialClick}name="google">Continue with Google</button>
-        <button onClick={onSocialClick}name="github">Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
